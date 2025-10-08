1afec3bceee68dd709c98008278e382be3387155
kainamf <kaina.marques@calindra.com.br>
2025-10-08 12:05:35 -0300

    refactor: simplify parameter handling in ProductInfiniteScroll component

--- shopping-vtex-template-home/src/components/CmsComponents/ProductInfiniteScroll/ProductInfiniteScroll.jsx
+++ shopping-vtex-template-home/src/components/CmsComponents/ProductInfiniteScroll/ProductInfiniteScroll.jsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react'
-import { View, Text } from 'eitri-luminus'
 import ProductCatalogContent from '../../ProductCatalogContent/ProductCatalogContent'
 
 export default function ProductInfiniteScroll(props) {
@@ -8,28 +6,13 @@ export default function ProductInfiniteScroll(props) {
 	const [params, setParams] = useState(null)
 
 	useEffect(() => {
-		if (data) {
-			// Garantir que facets seja sempre um array válido
-			let facets = []
-			if (Array.isArray(data.facets)) {
-				facets = data.facets
-			} else if (data.facets && typeof data.facets === 'object') {
-				// Se facets não for array mas for um objeto, tentar converter
-				facets = []
-			}
-
-			// Verificar diferentes possíveis propriedades para o termo de busca
-			const searchTerm = data.term || data.query || data.search || data.searchTerm || data.keyword || ''
-
-			const searchParams = {
-				facets: facets,
-				query: searchTerm,
-				sort: data.sort ?? ''
-			}
-			
-			setParams(searchParams)
+		const params = {
+			facets: data.facets || [],
+			query: data.term ?? '',
+			sort: data.sort ?? ''
 		}
-	}, [data])
+		setParams(data)
+	}, [])
 
 	return (
 		<View>
@@ -38,12 +21,10 @@ export default function ProductInfiniteScroll(props) {
 					<Text className='font-bold text-xl'>{data?.title}</Text>
 				</View>
 			)}
-			{params && (
-				<ProductCatalogContent
-					params={params}
-					hideFilters
-				/>
-			)}
+			<ProductCatalogContent
+				params={params}
+				hideFilters
+			/>
 		</View>
 	)
 }
