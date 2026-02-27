7c91959d59b4d696b0e1df527ad89620e517ecb4
Igor Cantelmo <igor.cantelmo@calindra.com.br>
2026-02-27 18:32:43 -0300

    some adjustments

--- shopping-vtex-template-home/src/components/ShelfOfProducts/ShelfOfProducts.jsx
+++ shopping-vtex-template-home/src/components/ShelfOfProducts/ShelfOfProducts.jsx
@@ -1,11 +1,11 @@
-import { Text, View } from 'eitri-luminus'
+import { Skeleton, Text, View } from 'eitri-luminus'
 import ShelfOfProductsCarousel from './components/ShelfOfProductsCarousel'
 import ShelfOfProductsSlider from './components/ShelfOfProductsSlider'
 import Eitri from 'eitri-bifrost'
 import { useTranslation } from 'eitri-i18n'
 
 export default function ShelfOfProducts(props) {
-	const { products, title, isLoading, mode, searchParams, ...rest } = props
+	const { products, title, isLoading = true, mode, searchParams, ...rest } = props
 
 	const { t } = useTranslation()
 
@@ -23,12 +23,20 @@ export default function ShelfOfProducts(props) {
 		<View>
 			{title && (
 				<View className={`flex justify-between items-center px-4`}>
-					<Text className='font-bold text-xl'>{isLoading ? t('shelfOfProducts.loading') : title}</Text>
+					{isLoading ? (
+						<Skeleton className='!w-[160px] !h-7 !rounded-md' />
+					) : (
+						<Text className='font-bold text-xl'>{title}</Text>
+					)}
 					{searchParams && (
 						<View
 							onClick={seeMore}
 							className='flex items-center min-w-fit'>
-							<Text className='font-bold'>{t('shelfOfProducts.seeMore')}</Text>
+							{isLoading ? (
+								<Skeleton className='!w-[64px] !h-7 !rounded-md' />
+							) : (
+								<Text className='font-bold'>{t('shelfOfProducts.seeMore')}</Text>
+							)}
 						</View>
 					)}
 				</View>
